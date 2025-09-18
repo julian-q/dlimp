3ac779efbb39d5295de0890c098c06a5b7790b75
Julian Quevedo <julianhquevedo@gmail.com>
2025-09-18 16:44:33 -0700

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="dlimp",
-    version="0.0.1",
+    version="0.0.2",
     packages=find_packages(),
     python_requires=">=3.8",
     install_requires=[
